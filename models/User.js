8c0e6895c3ebb30d9abb5b351a73655a36aea8ab
hazem <hazemjh10@gmail.com>
2024-01-10 07:07:04 +0100

    first commit

---
+++ models/User.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+// User schema
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  age: { type: Number, required: true },
+  profession: { type: String, default: "batal" },
+});
+
+// User model
+const User = mongoose.model("User", UserSchema);
+
+export default User;
